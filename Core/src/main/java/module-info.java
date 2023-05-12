d133d0356c67bf99521ce0691b7be175b4fb6bca
Kristian Kanstrup <krjoh21@student.sdu.dk>
2023-05-12 15:15:42 +0200

    Spring imp

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,13 +1,23 @@
 module Core {
+    //Asteroids internal modules
     requires Common;
     requires CommonEnemy;
     requires CommonBullet;
     requires CommonAsteroids;
+
+    //Spring & LibGDX
     requires java.desktop;
-    requires com.badlogic.gdx;   
+    requires com.badlogic.gdx;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.core;
+    exports dk.sdu.mmmi.cbse.main;
+    opens dk.sdu.mmmi.cbse.main to spring.core;
+
+    //Asteroids SPIs
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
-    uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+    uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 }
 
 
